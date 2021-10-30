50f8ecf9c8160f9afb5366d399bb0836aad86fa8
marufahamad <maruf.ahamad@gmail.com>
2021-10-31 00:53:13 +0600

    almost done. commit before update setting for bonus task 3

--- index.js
+++ index.js
@@ -21,12 +21,28 @@ async function run() {
 
         const database = client.db("bdTourPlanner");
         const toursCollection = database.collection('tours');
+        const bookingsCollection = database.collection('bookings')
+        const reviewsCollection = database.collection("reviews")
 
-        // get API
+        // get tour API
         app.get('/tours', async (req, res) => {
             const cursor = toursCollection.find({});
             const tours = await cursor.toArray();
             res.send(tours);
+        })
+
+        // get booking API
+        app.get('/bookings', async (req, res) => {
+            const cursor = bookingsCollection.find({});
+            const booking = await cursor.toArray();
+            res.send(booking);
+        })
+
+        // get reviews API
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({})
+            const review = await cursor.toArray();
+            res.send(review);
         })
 
         // get single Tour Location
@@ -37,6 +53,43 @@ async function run() {
             res.json(tour);
         })
 
+
+        // post (add) a new tour in API
+        app.post('/tours', async (req, res) => {
+            const tour = req.body;
+            const result = await toursCollection.insertOne(tour);
+            res.json(result);
+        })
+
+
+        // post booking API
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log('hitting the post', booking);
+
+            const result = await bookingsCollection.insertOne(booking);
+            res.json(result);
+        })
+
+        // post reviews API
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.json(result);
+
+        })
+
+        // update booking status in API
+
+
+        // delete single booking API
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingsCollection.deleteOne(query);
+            res.json(result);
+        })
     }
     finally {
         // await client.close()
